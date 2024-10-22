3d6b0e387578cc3eed2b83f68e8a8aa222787bb2
Sai Vardhan <vardhan.6405@gmail.com>
2024-10-22 23:41:34 +0530

    feat: playlist routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRouter from "./auth";
+import playlistRouter from "./playlist";
 
 const router: Router = Router();
 
 router.use(authRouter);
+router.use(playlistRouter);
 
 export default router;
---
+++ server/src/routes/playlist.ts
@@ -0,0 +1,251 @@
+import { Router, Request, Response, NextFunction } from "express";
+import pool from "../db";
+import { aucthenticateJWT } from "../util/authHelpers";
+const router: Router = Router();
+
+// to CREATE a playlist
+router.post(
+  "/api/playlist",
+  aucthenticateJWT,
+  (req: Request, res: Response, next: NextFunction) => {
+    const { name, description, is_private } = req.body;
+
+    if (req.user === null) {
+      res.status(401).send({
+        message: "Invalid token or no token provided",
+      });
+      return;
+    }
+    if (
+      name === undefined ||
+      name === null ||
+      is_private === null ||
+      is_private === undefined
+    ) {
+      res.status(400).send({
+        message: "Missing necessary details",
+      });
+      return;
+    }
+    if (typeof name !== "string" || name.length > 16) {
+      //TODO after updating database: 16 to 32
+      res.status(400).send({
+        message: "Invalid playlist name",
+      });
+      return;
+    }
+    if (typeof description === "string" && description.length > 32) {
+      //TODO after updating database: 32 to 256
+      res.status(400).send({
+        message: "Invalid playlist description",
+      });
+      return;
+    }
+
+    next();
+  },
+  async (req: Request, res: Response) => {
+    const { name, description, is_private } = req.body;
+
+    try {
+      const newPlaylist = await pool.query(
+        "INSERT INTO playlists(name, description, owner_id, is_private) VALUES($1, $2, $3, $4);",
+        [name, description ?? null, req.user?.id, is_private]
+      );
+
+      if (Number(newPlaylist?.rowCount) > 0) res.sendStatus(201);
+      else {
+        res.status(400).send({
+          message: "Could not create playlist",
+        });
+      }
+    } catch (error) {
+      console.log("Error at POST /api/playlist route:\n", error);
+      res.sendStatus(500);
+    }
+  }
+);
+
+// to DELETE a playlist
+router.delete(
+  "/api/playlist",
+  aucthenticateJWT,
+  (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.body;
+    if (id === null || id === undefined) {
+      res.status(400).send({
+        message: "Missing necessary details",
+      });
+      return;
+    }
+
+    next();
+  },
+  async (req: Request, res: Response, next: NextFunction) => {
+    if (req.user === null) {
+      res.status(401).send({
+        message: "Invalid token or no token provided",
+      });
+      return;
+    }
+
+    const { id } = req.body;
+
+    try {
+      const deletedPlaylist = await pool.query(
+        "DELETE FROM playlists WHERE id = $1;",
+        [id]
+      );
+
+      if (Number(deletedPlaylist?.rowCount) > 0) res.sendStatus(200);
+      else {
+        res.status(400).send({
+          message: "Could not delete playlist",
+        });
+      }
+    } catch (error) {
+      console.log("Error at DELETE /api/playlist route:\n", error);
+      res.sendStatus(500);
+    }
+  }
+);
+
+// to READ details of a playlist
+router.get(
+  "/api/playlist-details",
+  (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.query;
+    if (id === null || id === undefined) {
+      res.status(400).send({
+        message: "Missing necessary details",
+      });
+      return;
+    }
+
+    next();
+  },
+  aucthenticateJWT,
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.query;
+
+    try {
+      const playlist = await pool.query(
+        `SELECT p.id, p.name,p.description,p.cover_image,p.owner_id,p.total_playtime, p.track_count, p.is_private ,u.username as owner_username 
+        FROM playlists p 
+        INNER JOIN users u ON u.id = p.owner_id 
+        WHERE p.id = $1;`,
+        [id]
+      );
+
+      if (Number(playlist.rowCount) > 0) {
+        // for private playlists checking for ownership
+        if (playlist.rows[0].is_private) {
+          if (req.user !== null && req.user.id === playlist.rows[0].owner_id)
+            res.status(200).send({ ...playlist.rows[0] });
+          else
+            res
+              .status(401)
+              .send({ message: "This playlist is set to private" });
+        }
+        // public playlists have no ownership check
+        else res.status(200).send({ ...playlist.rows[0] });
+      } else {
+        res.status(400).send({
+          message: "Could not read playlist details",
+        });
+      }
+    } catch (error) {
+      console.log("Error at GET /api/playlist-details route:\n", error);
+      res.sendStatus(500);
+    }
+  }
+);
+// to READ song list of a playlist
+router.get(
+  "/api/playlist-songs",
+  (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.query;
+    if (id === null || id === undefined) {
+      res.status(400).send({
+        message: "Missing necessary details",
+      });
+      return;
+    }
+
+    next();
+  },
+  aucthenticateJWT,
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.query;
+
+    try {
+      const playlist = await pool.query(
+        "SELECT * FROM playlists WHERE id = $1;",
+        [id]
+      );
+      const playlistSongs = await pool.query(
+        `SELECT s.id, s.name, s.playtime, jsonb_agg(jsonb_build_object('id',a.id,'name',a.name)) as artists
+        FROM playlists_songs ps 
+        INNER JOIN songs s ON s.id = ps.song_id 
+        INNER JOIN artists_songs ars ON ars.song_id = s.id 
+        INNER JOIN artists a ON a.id = ars.artist_id 
+        WHERE playlist_id = $1 
+        GROUP BY s.id,s.name,s.playtime`,
+        [id]
+      );
+
+      if (Number(playlist.rowCount) > 0) {
+        // for private playlists checking for ownership
+        if (playlist.rows[0].is_private) {
+          if (req.user !== null && req.user.id === playlist.rows[0].owner_id)
+            res.status(200).send({ songs: playlistSongs.rows });
+          else
+            res
+              .status(401)
+              .send({ message: "This playlist is set to private" });
+        }
+        // public playlists have no ownership check
+        else res.status(200).send({ songs: playlistSongs.rows });
+      } else {
+        res.status(400).send({
+          message: "Could not read playlist songs",
+        });
+      }
+    } catch (error) {
+      console.log("Error at GET /api/playlist-songs route:\n", error);
+      res.sendStatus(500);
+    }
+  }
+);
+
+// to Read list of user's songs
+router.get(
+  "/api/user-playlists",
+  aucthenticateJWT,
+  async (req: Request, res: Response, next: NextFunction) => {
+    if (req.user === null) {
+      res.status(401).send({
+        message: "Invalid token or no token provided",
+      });
+      return;
+    }
+
+    try {
+      const playlists = await pool.query(
+        "SELECT * FROM playlists WHERE owner_id = $1;",
+        [req.user.id]
+      );
+      if (Number(playlists.rowCount) > 0)
+        res.status(200).send({ playlists: playlists.rows });
+      else
+        res.status(400).send({
+          message: "Could not read user's playlist data",
+        });
+    } catch (error) {
+      console.log("Error at GET /api/user-playlists route:\n", error);
+      res.sendStatus(500);
+    }
+  }
+);
+
+export default router;
